3a099fe65fde4841f85c858affdbcbde4b130ee7
Luan-Macedo <79669889+Luan-Macedo@users.noreply.github.com>
2022-10-19 17:24:18 -0300

    Commit inicial

---
+++ main.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+    $('form').on('submit', function(e){
+        e.preventDefault();
+
+        const addTarefa = $('#adicionar-tarefa').val();
+        const novaTarefa = $('<li><a href=""></a></li>')
+
+        $(`<li><a href="">${addTarefa}</a></li>`).appendTo(novaTarefa);
+        $(novaTarefa).appendTo('ul');
+
+        $('#adicionar-tarefa').val('');
+        
+        $(novaTarefa).click(function(e){
+            e.preventDefault();
+    
+            $(novaTarefa).fadeOut();
+        })
+    })
+
+})
